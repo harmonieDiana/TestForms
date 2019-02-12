555abd83cb17881867db60f57399085ae705daf7
harmonieDiana <diana.klebanovich@gmail.com>
2019-02-12 14:56:27 +0300

    Commit message

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -5,6 +5,7 @@ public class SignUpPage {
     WebDriver driver;
 
     public SignUpPage (WebDriver driver){
+
         this.driver = driver;
     }
 
@@ -13,6 +14,21 @@ public class SignUpPage {
     By emailField = By.xpath("//*[@id=\"zakaz_compred\"]/input[9]");
     By checkBox = By.id("261506110b51b1b313f02a2fe7df28dd");
     By submitButton = By.id("zakaz_compred_send");
+    By callbackButton = By.className("popup-with-zoom-anim");
+    By nameFieldHeader = By.xpath("//*[@id=\"header_zakaz_zvonka\"]/input[6]");
+    By telFieldHeader = By.xpath("//*[@id=\"header_zakaz_zvonka\"]/input[8]");
+    By checkBoxHeader = By.id("c17c851ef98311f3fb2856742cf54011");
+    By submitButtonHeader = By.id("header_zakaz_zvonka_send");
+    By nameFieldKonsult = By.xpath("//*[@id=\"poluchit_konsult\"]/input[6]");
+    By telFieldKonsult = By.xpath("//*[@id=\"poluchit_konsult\"]/input[8]");
+    By emailFieldKonsult = By.xpath("//*[@id=\"poluchit_konsult\"]/input[9]");
+    By checkBoxKonsult = By.id("212970185");
+    By submitButtonKonsult = By.id("poluchit_konsult_send");
+    By nameFieldBottom = By.xpath("//*[@id=\"otpravka_zayavki\"]/div/div[1]/div[1]/input");
+    By telFieldBottom = By.xpath("//*[@id=\"otpravka_zayavki\"]/div/div[1]/div[2]/input");
+    By emailFieldBottom = By.xpath("//*[@id=\"otpravka_zayavki\"]/div/div[1]/div[3]/input");
+    By checkBoxBottom = By.id("25be56aa945d2dbd2341a662b0d25f00");
+    By submitButtonBottom = By.id("otpravka_zayavki_send");
 
     public SignUpPage typeName(String name){
         driver.findElement(nameField).sendKeys(name);
@@ -37,6 +53,12 @@ public class SignUpPage {
     public SignUpPage clickSubmitButton(){
         driver.findElement(submitButton).click();
         return this;
+    }
+
+    public SignUpPage clickCallbackButton(){
+        driver.findElement(callbackButton).click();
+        return this;
+
     }
 
 }
--- src/test/java/SignUpPageTest.java
+++ src/test/java/SignUpPageTest.java
@@ -1,6 +1,8 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,8 +11,8 @@ import java.util.concurrent.TimeUnit;
 
 public class SignUpPageTest {
 
-    private WebDriver driver;
-    private SignUpPage signUpPage;
+    WebDriver driver;
+    SignUpPage signUpPage;
 
     @Before
     public void setUp(){
@@ -24,10 +26,12 @@ public class SignUpPageTest {
 
     @Test
     public void TypeName(){
-        SignUpPage newSignUpPage = signUpPage.typeName("test")
+        SignUpPage newSignUpPage = signUpPage
+                //.typeName("test")
                 .typeEmail("test@test.ru")
                 .typeTel("111111111")
                 .clickSubmitButton();
+        Assert.assertTrue(signUpPage.driver.findElement(By.className("error ")).isDisplayed());
     }
 
     @After
