d7bc93605c97892dc6d9ba0e88f9b79d40b607b8
Диана <diana.klebanovich@gmail.com>
2019-05-02 12:36:10 +0300

    message

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+
+    By NameField = By.xpath("//*[@id=\"comp_7e933bd22693dbdd47b10c3780b14858\"]/section/div/div/form/div/div[1]/div[1]/input");
+    By TelField = By.xpath("//*[@id=\"comp_7e933bd22693dbdd47b10c3780b14858\"]/section/div/div/form/div/div[1]/div[2]/input");
+    By EmailField = By.xpath("//*[@id=\"comp_7e933bd22693dbdd47b10c3780b14858\"]/section/div/div/form/div/div[1]/div[3]/input");
+    By CheckBox = By.id("261506110b51b1b313f02a2fe7df28dd");
+    By SubmitButton = By.className("butt");
+
+    public MainPage open(){
+        Selenide.open("/digital-contacts.html");
+        return this;
+    }
+
+    public MainPage rightForm (String name, String tel, String email){
+        $(NameField).setValue(name);
+        $(TelField).setValue(tel);
+        $(EmailField).setValue(email);
+        $(SubmitButton).click();
+        return this;
+    }
+}
---
+++ src/test/java/FormsTest.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browser;
+
+public class FormsTest {
+    MainPage mainPage;
+
+    @BeforeClass
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+        Configuration.timeout = 10000;
+        Configuration.startMaximized = true;
+        baseUrl = "https://www.artox-media.ru";
+        browser = "chrome";
+    }
+
+    @Test
+    public void EmptyRightForm(){
+        mainPage = new MainPage();
+        mainPage.open();
+        mainPage.rightForm("", "", "");
+        $(By.className("red")).shouldHave(Condition.text("Поле Контактный телефон не заполнено"));
+    }
+}
